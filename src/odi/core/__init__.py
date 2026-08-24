7650aabe7c59f72af5008e939d429e5650c9c89c
Niceone Obsidian <niceoneobsidian@gmail.com>
2026-08-23 22:44:30 -0400

    chore: initialize core package

---
+++ src/odi/core/__init__.py
@@ -0,0 +1,5 @@
+"""ODI core domain primitives."""
+
+from .contracts import CapabilityContract, ExecutionContext, LifecycleState, RiskClass
+
+__all__ = ["CapabilityContract", "ExecutionContext", "LifecycleState", "RiskClass"]
